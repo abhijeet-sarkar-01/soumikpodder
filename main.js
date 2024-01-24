54753601896e6f3ae7b8c91a905a9ee5dbcb0e75
Abhijeet Sarkar <abhijeet.sarkar7191@gmail.com>
2024-01-24 18:07:14 +0530

    Add the remaining files

--- main.js
+++ main.js
@@ -6,3 +6,32 @@ for (let i = 0; i < links.length; i++) {
         links[i].classList.add("nav-active");
     }
 }
+
+document.querySelector(".red-col").addEventListener("click", ()=>{
+    document.body.className = ''
+    document.body.classList.add("red");
+})
+document.querySelector(".blue-col").addEventListener("click", ()=>{
+    document.body.className = ''
+    document.body.classList.add("blue");
+})
+document.querySelector(".green-col").addEventListener("click", ()=>{
+    document.body.className = ''
+    document.body.classList.add("green");
+})
+document.querySelector(".peach-col").addEventListener("click", ()=>{
+    document.body.className = ''
+    document.body.classList.add("peach");
+})
+document.querySelector(".lav-col").addEventListener("click", ()=>{
+    document.body.className = ''
+    document.body.classList.add("lavender");
+})
+document.querySelector(".grey-col").addEventListener("click", ()=>{
+    document.body.className = ''
+    document.body.classList.add("grey");
+})
+document.querySelector(".default-col").addEventListener("click", ()=>{
+    document.body.className = ''
+    document.body.classList.add("default");
+})
